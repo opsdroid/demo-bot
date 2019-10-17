69e2df0fedbe4f7871d3589d5de60a305daf9616
Jacob Tomlinson <jtomlinson@nvidia.com>
2019-10-17 09:30:47 +0100

    Switch keepalive to use aiohttp and turn on debug logging

--- skills/heroku/__init__.py
+++ skills/heroku/__init__.py
@@ -1,15 +1,14 @@
 from opsdroid.skill import Skill
 from opsdroid.matchers import match_crontab
 
-import requests
+import aiohttp
 
 
 class HerokuSkill(Skill):
 
     @match_crontab('*/5 * * * *')
     async def keepalive(self, message):
-        requests.get(
-            'https://{app_name}.herokuapp.com/stats'.format(
+        async with aiohttp.ClientSession() as session:
+            await session.get('https://{app_name}.herokuapp.com/stats'.format(
                 app_name=self.config.get('app-name')
-                )
-            )
+            ))
